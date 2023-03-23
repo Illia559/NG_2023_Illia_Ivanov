fdd9e0f6d4e1c7eccae66243c17b864251b20c5c
Illia559 <123123ilja@gmail.com>
2023-03-23 16:47:20 +0200

    correct main.py; complete with match case

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -1,17 +1,12 @@
 def operation(operat):
-    if operat == "+":
-        result = userNumber1 + userNumber2
-    elif operat == "-":
-        result = userNumber1 - userNumber2
-    elif operat == "*":
-        result = userNumber1 * userNumber2
-    elif operat == "/":
-        result = userNumber1 / userNumber2
-    return result
-
-
+    match operat:
+        case "+": print(userNumber1 + userNumber2)
+        case "-": print(userNumber1 - userNumber2)
+        case "*": print(userNumber1 * userNumber2)
+        case "/": print(userNumber1 / userNumber2)
+         
 
 userNumber1 = float(input("Enter first number: "))
 userNumber2 = float(input("Enter second number: "))
 oper =str(input("Enter operation: "))
-print (operation(oper))
+operation(oper)
