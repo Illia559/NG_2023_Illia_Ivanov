8365d1458deb141f949f0beda6267301e3fb0150
Illia559 <123123ilja@gmail.com>
2023-03-24 23:15:11 +0200

    correct main.py

--- Lection_1/Task_3/main.py
+++ Lection_1/Task_3/main.py
@@ -4,8 +4,7 @@ def operation(operat):
         case "-": print(userNumber1 - userNumber2)
         case "*": print(userNumber1 * userNumber2)
         case "/": print(userNumber1 / userNumber2)
-         
-
+                 
 userNumber1 = float(input("Enter first number: "))
 userNumber2 = float(input("Enter second number: "))
 oper =str(input("Enter operation: "))
